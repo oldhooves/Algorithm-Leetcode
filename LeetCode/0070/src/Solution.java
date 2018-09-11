d45d628513222acebb702edc6dcf637533cfffc7
laotizi <465304942@qq.com>
2018-09-11 22:07:20 +0800

    0070

---
+++ LeetCode/0070/src/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * 70. 爬楼梯
+ * https://leetcode-cn.com/problems/climbing-stairs/description/
+ * Created by 老蹄子 on 2018/9/11 下午10:04
+ */
+class Solution {
+
+
+    public int climbStairs(int n) {
+
+        int memo[] = new int[n + 1];
+        memo[1] = 1;
+        memo[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        return memo[n];
+    }
+}
