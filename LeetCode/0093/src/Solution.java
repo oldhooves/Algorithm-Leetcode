d53e21c901266de8df9fedc83b9d9bb4efdad84e
laotizi <465304942@qq.com>
2018-10-23 17:20:29 +0800

    0093 finished

---
+++ LeetCode/0093/src/Solution.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 93. 复原IP地址
+ * https://leetcode-cn.com/problems/restore-ip-addresses/description/
+ * Created by 老蹄子 on 2018/10/23 下午4:19
+ */
+class Solution {
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        int len = s.length();
+        for (int i = 1; i < 4 && i < len - 2; i++) {
+            for (int j = i + 1; j < i + 4 && j < len - 1; j++) {
+                for (int k = j + 1; k < j + 4 && k < len; k++) {
+                    String s1 = s.substring(0,i);
+                    String s2 = s.substring(i,j);
+                    String s3 = s.substring(j,k);
+                    String s4 = s.substring(k,len);
+                    if (isValid(s1) && isValid(s2) && isValid(s3) && isValid(s4)){
+                        res.add(s1 + "." + s2 + "." + s3 + "." + s4);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean isValid(String s){
+        if (s.length() > 3 || s.length() == 0 || (s.charAt(0) == '0' && s.length() > 1) || Integer.parseInt(s) > 255){
+            return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().restoreIpAddresses("25525511135"));
+    }
+}
