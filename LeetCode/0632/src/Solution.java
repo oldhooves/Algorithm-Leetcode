ae24186482bc06c86b2ab746084b7089407f8631
laotizi <465304942@qq.com>
2018-10-31 21:37:54 +0800

    0632 finished

---
+++ LeetCode/0632/src/Solution.java
@@ -0,0 +1,43 @@
+
+import java.util.List;
+import java.util.PriorityQueue;
+
+/**
+ * 632. 最小区间
+ * https://leetcode-cn.com/problems/smallest-range/description/
+ * Created by 老蹄子 on 2018/10/31 下午7:59
+ */
+class Solution {
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int minx = 0;
+        int miny = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        int[] next = new int[nums.size()];
+        boolean flag = true;
+        PriorityQueue<Integer> min_queue = new PriorityQueue<>((i,j) ->
+                nums.get(i).get(next[i]) - nums.get(j).get(next[j]));
+        for (int i = 0; i < nums.size(); i++) {
+            min_queue.offer(i);
+            max = Math.max(max,nums.get(i).get(0));
+        }
+
+        for (int i = 0; i < nums.size() && flag; i++) {
+            for (int j = 0; j < nums.get(i).size(); j++) {
+                int min_i = min_queue.poll();
+                if (miny - minx > max - nums.get(min_i).get(next[min_i])) {
+                    minx = nums.get(min_i).get(next[min_i]);
+                    miny = max;
+                }
+                next[min_i]++;
+                if (next[min_i] == nums.get(min_i).size()) {
+                    flag = false;
+                    break;
+                }
+                min_queue.offer(min_i);
+                max = Math.max(max,nums.get(min_i).get(next[min_i]));
+            }
+        }
+        return new int[] {minx,miny};
+    }
+
+}
