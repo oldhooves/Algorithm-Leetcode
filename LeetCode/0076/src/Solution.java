60d652b394a95db9ea18bfcc0cf10ad80a00c68b
laotizi <465304942@qq.com>
2018-10-31 16:52:52 +0800

    0076 finished

--- LeetCode/0076/src/Solution.java
+++ LeetCode/0076/src/Solution.java
@@ -53,6 +53,6 @@ class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().minWindow("ADOBECODEBANC","ABC"));
+        System.out.println(new Solution().minWindow("ABAACBAB","ABC"));
     }
 }
