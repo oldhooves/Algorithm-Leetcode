bf31ebdded59c925bc437efc291248403ac135c1
laotizi <465304942@qq.com>
2018-11-02 15:57:57 +0800

    0010 finished

---
+++ LeetCode/0010/src/Solution.java
@@ -0,0 +1,43 @@
+/**
+ * 10. 正则表达式匹配
+ * https://leetcode-cn.com/problems/regular-expression-matching/description/
+ * Created by 老蹄子 on 2018/11/2 下午1:52
+ */
+class Solution {
+    public boolean isMatch(String s, String p) {
+
+        if (s == null || p == null) {
+            return false;
+        }
+
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int i = 0; i < p.length(); i++) {
+            if (p.charAt(i) == '*' && dp[0][i - 1]){
+                dp[0][i + 1] = true;
+            }
+        }
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < p.length(); j++) {
+                if (p.charAt(j) == '.') {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == s.charAt(i)) {
+                    dp[i + 1][j + 1] = dp[i][j];
+                }
+                if (p.charAt(j) == '*') {
+                    if (p.charAt(j - 1) != s.charAt(i) && p.charAt(j - 1) != '.') {
+                        dp[i + 1][j + 1] = dp[i + 1][j - 1];
+                    }else {
+                        dp[i + 1][j + 1] = (dp[i + 1][j] || dp[i + 1][j - 1] || dp[i][j + 1]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().isMatch("aab","c*a*b"));
+    }
+}
