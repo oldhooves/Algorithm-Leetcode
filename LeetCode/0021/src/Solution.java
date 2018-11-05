f324676ae440bd0b90771708b9600c63337776be
laotizi <465304942@qq.com>
2018-11-05 10:34:15 +0800

    0021 finished

---
+++ LeetCode/0021/src/Solution.java
@@ -0,0 +1,51 @@
+/**
+ * 21. 合并两个有序链表
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/description/
+ * Created by 老蹄子 on 2018/11/5 上午10:15
+ */
+class Solution {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode temp = new ListNode(-1);
+        ListNode result = temp;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+                temp = temp.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+                temp = temp.next;
+            }
+        }
+        if (l1 == null) {
+            temp.next = l2;
+        }
+        if (l2 == null) {
+            temp.next = l1;
+        }
+        return result.next;
+    }
+
+
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+        System.out.println(Solution.mergeTwoLists(l1,l2));
+    }
+}
