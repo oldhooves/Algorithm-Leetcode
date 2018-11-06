1853453f48250acbda9c3a1a0bab0e2f144111aa
laotizi <465304942@qq.com>
2018-11-06 20:29:11 +0800

    0002 finished

---
+++ LeetCode/0002/src/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * 2. 两数相加
+ * https://leetcode-cn.com/problems/add-two-numbers/description/
+ * Created by 老蹄子 on 2018/11/6 下午8:17
+ */
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode p = l1,q = l2,cur = dummy;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
+            if (p != null){
+                p = p.next;
+            }
+            if (q != null) {
+                q = q.next;
+            }
+        }
+        if (carry > 0) {
+            cur.next = new ListNode(carry);
+        }
+        return dummy.next;
+    }
+}
