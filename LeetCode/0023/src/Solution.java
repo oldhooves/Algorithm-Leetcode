4507379675308c4322f754796c5ed272651cddad
laotizi <465304942@qq.com>
2018-11-07 14:41:56 +0800

    0023 finished

---
+++ LeetCode/0023/src/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+/**
+ * 23. 合并K个排序链表
+ * https://leetcode-cn.com/problems/merge-k-sorted-lists/description/
+ * Created by 老蹄子 on 2018/11/7 下午2:19
+ */
+class Solution {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                if (o1.val < o2.val) {
+                    return -1;
+                } else if (o1.val == o2.val) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                queue.add(node);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            tail.next = queue.poll();
+            tail = tail.next;
+            if (tail.next != null) {
+                queue.add(tail.next);
+            }
+        }
+        return dummy.next;
+    }
+}
