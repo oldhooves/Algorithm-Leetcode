d318b1428ba8bbf3b3ee3203721559d6df0522e7
laotizi <465304942@qq.com>
2018-11-09 14:30:51 +0800

    0877 finished

---
+++ LeetCode/0877/src/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * 877. 石子游戏
+ * https://leetcode-cn.com/problems/stone-game/description/
+ * Created by 老蹄子 on 2018/11/9 下午2:12
+ */
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
