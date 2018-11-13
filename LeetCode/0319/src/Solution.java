acccecb66aa930d077a77c9a38e1fff69178ff0a
laotizi <465304942@qq.com>
2018-11-13 14:33:51 +0800

    0319 finished

---
+++ LeetCode/0319/src/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * 319. 灯泡开关
+ * https://leetcode-cn.com/problems/bulb-switcher/description/
+ * Created by 老蹄子 on 2018/11/13 下午2:31
+ */
+class Solution {
+    public int bulbSwitch(int n) {
+       return (int)Math.sqrt(n);
+    }
+}
