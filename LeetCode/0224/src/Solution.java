d89a0b8ff9a7898979be04ec11697161bbbf0b12
laotizi <465304942@qq.com>
2018-11-15 15:03:41 +0800

    0224 finished

---
+++ LeetCode/0224/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.Stack;
+
+/**
+ * 224. 基本计算器
+ * https://leetcode-cn.com/problems/basic-calculator/description/
+ * Created by 老蹄子 on 2018/11/15 下午2:17
+ */
+class Solution {
+    public int calculate(String s) {
+        int len = s.length();
+        int sign = 1;
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i  + 1) - '0';
+                    i++;
+                }
+                result += sum * sign;
+            } else if (s.charAt(i) == '+') {
+                sign = 1;
+            } else if (s.charAt(i) == '-') {
+                sign = -1;
+            } else if (s.charAt(i) == '(') {
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().calculate("(1+(4+5+2)-3)+(6+8)"));
+    }
+}
