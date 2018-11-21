46edc95f26f232590bca6580d7d6802c4bf68a67
laotizi <465304942@qq.com>
2018-11-21 15:20:26 +0800

    0438 finished

---
+++ LeetCode/0438/src/Solution.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+/**
+ * 438. 找到字符串中所有字母异位词
+ * https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/description/
+ * Created by 老蹄子 on 2018/11/20 下午10:07
+ */
+class Solution {
+    public List<Integer> findAnagrams(String s, String t) {
+        List<Integer> result = new LinkedList<>();
+        if(t.length()> s.length()) return result;
+        Map<Character, Integer> map = new HashMap<>();
+        for(char c : t.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        int counter = map.size();
+
+        int begin = 0, end = 0;
+        int head = 0;
+        int len = Integer.MAX_VALUE;
+
+
+        while(end < s.length()){
+            char c = s.charAt(end);
+            if( map.containsKey(c) ){
+                map.put(c, map.get(c)-1);
+                if(map.get(c) == 0) counter--;
+            }
+            end++;
+
+            while(counter == 0){
+                char tempc = s.charAt(begin);
+                if(map.containsKey(tempc)){
+                    map.put(tempc, map.get(tempc) + 1);
+                    if(map.get(tempc) > 0){
+                        counter++;
+                    }
+                }
+                if(end-begin == t.length()){
+                    result.add(begin);
+                }
+                begin++;
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().findAnagrams("cbaebabacd","abc"));
+    }
+}
