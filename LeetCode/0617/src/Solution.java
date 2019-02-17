e70dd9234e874a3191064caf0080bfecb706133e
laotizi <465304942@qq.com>
2019-02-17 21:47:47 +0800

    0617 finished

---
+++ LeetCode/0617/src/Solution.java
@@ -0,0 +1,29 @@
+/**
+ * 617. 合并二叉树
+ * https://leetcode-cn.com/problems/merge-two-binary-trees/description/
+ * Created by 老蹄子 on 2019/2/17 下午9:42
+ */
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if (t1 == null) {
+            return t2;
+        }
+        if (t2 == null) {
+            return t1;
+        }
+        TreeNode node = new TreeNode(t1.val + t2.val);
+        node.left = mergeTrees(t1.left,t2.left);
+        node.right = mergeTrees(t1.right,t2.right);
+        return node;
+    }
+}
