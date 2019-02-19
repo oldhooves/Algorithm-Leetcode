d4625cd17801a3b3f01aae16a35323a0b6b7a1a0
laotizi <465304942@qq.com>
2019-02-19 21:24:13 +0800

    0107 finished

---
+++ LeetCode/0107/src/Solution.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 107. 二叉树的层次遍历 II
+ * https://leetcode-cn.com/problems/binary-tree-level-order-traversal-ii/description/
+ * Created by 老蹄子 on 2019/2/19 下午9:11
+ */
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (root == null) {
+            return ret;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ret.add(0,list);
+        }
+        return ret;
+    }
+}
