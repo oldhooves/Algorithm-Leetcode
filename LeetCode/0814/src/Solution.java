665992dc91f8252ed245a42840dad6a51b50332c
laotizi <465304942@qq.com>
2019-02-26 22:03:37 +0800

    0814 finished

---
+++ LeetCode/0814/src/Solution.java
@@ -0,0 +1,36 @@
+/**
+ * 814. 二叉树剪枝
+ * https://leetcode-cn.com/problems/binary-tree-pruning/description/
+ * Created by 老蹄子 on 2019/2/26 下午8:42
+ */
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public TreeNode pruneTree(TreeNode root) {
+        return containsOne(root) ? root : null;
+    }
+
+    private boolean containsOne(TreeNode node) {
+        if (node == null) {
+            return false;
+        }
+        boolean a1 = containsOne(node.left);
+        boolean a2 = containsOne(node.right);
+        if (!a1) {
+            node.left = null;
+        }
+        if (!a2) {
+            node.right = null;
+        }
+        return node.val == 1 || a1 || a2;
+    }
+}
