b073434aaa1e0264606e87b273afd9afad9f4489
laotizi <465304942@qq.com>
2019-03-03 19:30:26 +0800

    0662 finished

---
+++ LeetCode/0662/src/Solution.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 662. 二叉树最大宽度
+ * https://leetcode-cn.com/problems/maximum-width-of-binary-tree/description/
+ * Created by 老蹄子 on 2019/3/1 下午9:45
+ */
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root,0,1,new ArrayList<Integer>(),new ArrayList<Integer>());
+    }
+
+    private int dfs(TreeNode root, int level, int order, List<Integer> start, List<Integer> end) {
+        if (root == null) {
+            return 0;
+        }
+        if (start.size() == level) {
+            start.add(order);
+            end.add(order);
+        }
+        else {
+            end.set(level,order);
+        }
+        int cur = end.get(level) - start.get(level) + 1;
+        int left = dfs(root.left,level + 1,2 * order,start,end);
+        int right = dfs(root.right,level + 1,2 * order + 1,start,end);
+        return  Math.max(cur,Math.max(left,right));
+    }
+}
