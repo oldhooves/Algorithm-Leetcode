46ee79b13747d79f9d77e6525a2628af89e05f3e
laotizi <465304942@qq.com>
2019-03-03 20:24:23 +0800

    0116 finished

---
+++ LeetCode/0116/src/Solution.java
@@ -0,0 +1,38 @@
+/**
+ * 116. 填充每个节点的下一个右侧节点指针
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/description/
+ * Created by 老蹄子 on 2019/3/3 下午7:58
+ */
+class Solution {
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {}
+
+        public Node(int _val,Node _left,Node _right,Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left != null) {
+            root.left.next = root.right;
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
+        }
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+}
