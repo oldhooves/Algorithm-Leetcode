58e2cd30130bf991dde5be3e35c05730721c7544
laotizi <465304942@qq.com>
2019-03-06 22:13:48 +0800

    0124 finished

---
+++ LeetCode/0124/src/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * 124. 二叉树中的最大路径和
+ * https://leetcode-cn.com/problems/binary-tree-maximum-path-sum/description/
+ * Created by 老蹄子 on 2019/3/6 下午10:11
+ */
+class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    int maxValue;
+
+    public int maxPathSum(TreeNode root) {
+        maxValue = Integer.MIN_VALUE;
+        maxPathDown(root);
+        return maxValue;
+    }
+
+    private int maxPathDown(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = Math.max(0,maxPathDown(node.left));
+        int right = Math.max(0,maxPathDown(node.right));
+        maxValue = Math.max(maxValue,left + right + node.val);
+        return Math.max(left,right) + node.val;
+    }
+}
