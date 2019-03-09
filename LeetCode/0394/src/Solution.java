b88c4cb359b46a072d505533157bd8b891322d77
laotizi <465304942@qq.com>
2019-03-09 21:20:33 +0800

    0394 finished

---
+++ LeetCode/0394/src/Solution.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+/**
+ * 394. 字符串解码
+ * https://leetcode-cn.com/classic/problems/decode-string/description/
+ * Created by 老蹄子 on 2019/3/9 下午8:55
+ */
+class Solution {
+    public String decodeString(String s) {
+        String res = "";
+        Stack<Integer> countStack = new Stack<>();
+        Stack<String> resStack = new Stack<>();
+        int idx = 0;
+        while (idx < s.length()) {
+            if (Character.isDigit(s.charAt(idx))) {
+                int count = 0;
+                while (Character.isDigit(s.charAt(idx))) {
+                    count = 10 * count + (s.charAt(idx) - '0');
+                    idx++;
+                }
+                countStack.add(count);
+            } else if (s.charAt(idx) == '[') {
+                resStack.push(res);
+                res = "";
+                idx++;
+            } else if (s.charAt(idx) == ']') {
+                StringBuilder temp = new StringBuilder(resStack.pop());
+                int repeat = countStack.pop();
+                for (int i = 0; i < repeat; i++) {
+                    temp.append(res);
+                }
+                res = temp.toString();
+                idx++;
+            } else {
+                res += s.charAt(idx++);
+            }
+        }
+        return res;
+    }
+}
