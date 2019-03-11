d5bddaeb0561b5d47701a7ce5f25e9ba61bd0b02
laotizi <465304942@qq.com>
2019-03-11 19:13:29 +0800

    0207 finished

---
+++ LeetCode/0207/src/Solution.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 207. 课程表
+ * https://leetcode-cn.com/classic/problems/course-schedule/description/
+ * Created by ${laotizi} on 2019-03-08.
+ */
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[][] matrix = new int[numCourses][numCourses];
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < prerequisites.length; i++) {
+            int ready = prerequisites[i][0];
+            int pre = prerequisites[i][1];
+            if (matrix[pre][ready] == 0) {
+                indegree[ready]++;
+            }
+            matrix[pre][ready] = 1;
+        }
+
+        int count = 0;
+        Queue<Integer> queue = new LinkedList<Integer>();
+        for (int i = 0; i < indegree.length; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+            for (int i = 0; i < numCourses; i++) {
+                if (matrix[course][i] != 0) {
+                    if (--indegree[i] == 0) {
+                        queue.offer(i);
+                    }
+                }
+            }
+        }
+        return count == numCourses;
+    }
+}
