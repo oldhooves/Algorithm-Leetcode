6ef4ff09a1a7782cc2f59da9f43599af99c4e2e5
laotizi <465304942@qq.com>
2019-03-19 19:44:39 +0800

    0944 finished

---
+++ LeetCode/0944/src/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * 944. 删列造序
+ * https://leetcode-cn.com/classic/problems/delete-columns-to-make-sorted/description/
+ * Created by 老蹄子 on 2019/3/19 下午7:37
+ */
+class Solution {
+    public int minDeletionSize(String[] A) {
+        int ans = 0;
+        for (int c = 0; c < A[0].length(); c++) {
+            for (int r = 0; r < A.length - 1; r++) {
+                if (A[r].charAt(c) > A[r + 1].charAt(c)) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
